6f2d2c8d14a083e8bb7869663f440c2d25f1e646
Adam Cuculich <cucupac1996@gmail.com>
2022-02-15 12:44:29 -0600

    Changed env vars.

--- app/settings.py
+++ app/settings.py
@@ -10,10 +10,15 @@ class Settings(BaseSettings):
     environment: str = "unknown"
     log_level: str = "info"
     server_host: str = "0.0.0.0"
-    server_port: int = 8000
+    server_port: int = 8624
     server_prefix: str = ""
 
-    database_url: str
+    db_username: str
+    db_password: str
+    db_host: str
+    db_port: str
+    db_name: str
+    db_engine: str
 
     token_url: str
     json_web_token_secret: str
