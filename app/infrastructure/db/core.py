65df2300b542d132918f012b022a3f8aaa36e672
ernesto1034 <ernesto1034@yahoo.com>
2022-02-17 14:26:39 -0600

    Removed TEST  print statements from core.py.

--- app/infrastructure/db/core.py
+++ app/infrastructure/db/core.py
@@ -9,7 +9,6 @@ async def get_or_create_database():
     global DATABASE
     if DATABASE is not None:
         return DATABASE
-    print("\n\n\n\n\n TEST PRINT: ", settings.json_web_token_algorithm, "\n\n\n\n\n\n")
 
     DATABASE = databases.Database(settings.db_url, min_size=5)
 
