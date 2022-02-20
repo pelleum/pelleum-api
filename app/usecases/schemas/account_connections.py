46d167152e4533903eb6501cba492764b57c591d
Adam Cuculich <cucupac1996@gmail.com>
2022-02-20 17:05:36 -0600

    Added new Pydantic validation on Account linking endpiont.

--- app/usecases/schemas/account_connections.py
+++ app/usecases/schemas/account_connections.py
@@ -18,19 +18,32 @@ class LoginRequest(BaseModel):
     )
 
 
-class MultiFactorAuthCodeRequest(BaseModel):
-    """JSON body model of MFA verify request"""
-
-    sms_code: Optional[constr(max_length=10)] = Field(
-        None,
+class MultiFactorWithChallenge(BaseModel):
+    sms_code: constr(max_length=10) = Field(
+        ...,
         description="The multifactor (sms) authentication code sent to the user's phone.",
         example="149837",
     )
-    challenge_id: Optional[constr(max_length=100)] = Field(
-        None,
+    challenge_id: constr(max_length=100) = Field(
+        ...,
         description="The unique identifier for the challenge that Robinhood issues to those who have 2FA disabled on their accounts.",
         example="ca3cf668-404c-49d2-8510-ea9948ff66aa",
     )
+
+
+class MultiFactorWithoutChallenge(BaseModel):
+    sms_code: constr(max_length=10) = Field(
+        ...,
+        description="The multifactor (sms) authentication code sent to the user's phone.",
+        example="149837",
+    )
+
+
+class MultiFactorAuthCodeRequest(BaseModel):
+    """Multi-factor authentication request from user"""
+
+    with_challenge: Optional[MultiFactorWithChallenge] = None
+    without_challenge: Optional[MultiFactorWithoutChallenge] = None
 
 
 class AccountConnectionsException(Exception):
